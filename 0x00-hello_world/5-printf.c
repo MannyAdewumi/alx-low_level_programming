c9d75ff87b07082cb561e08c5f27c14c026cdb17
MannyAdewumigit config --global user.name MannyAdewumi <mannyadewumi@gmail.com>
2024-06-20 14:22:42 -0700

    5-printf.c is done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- using printf to print line
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
