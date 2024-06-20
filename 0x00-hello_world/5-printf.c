3ec0bf7e884f343c9e7191f037193adde00998ed
MannyAdewumigit config --global user.name MannyAdewumi <mannyadewumi@gmail.com>
2024-06-20 14:39:05 -0700

    done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,9 @@
  * main- using printf to print line
  * Return: 0 (successful)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
-	return (0);
+	return(0);
 }
