829d8f10bb050cc315f218ba6bca102ccc036098
MannyAdewumigit config --global user.name MannyAdewumi <mannyadewumi@gmail.com>
2024-06-21 06:11:40 -0700

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - main function that prints line
+ * Return: 1 for success
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
+
+
