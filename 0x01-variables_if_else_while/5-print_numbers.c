984ad0644985d3ffe98d4f0a6633af48dc5f368f
MannyAdewumigit config --global user.name MannyAdewumi <mannyadewumi@gmail.com>
2024-06-22 18:41:05 -0700

    numbers in c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main f
+ * Return: 0 if succesful
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
