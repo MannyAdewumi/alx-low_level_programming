c420916c75feaa9b8d88b5f8669b732e64d967e9
MannyAdewumigit config --global user.name MannyAdewumi <mannyadewumi@gmail.com>
2024-06-28 08:27:28 -0700

    new printed triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,19 +11,24 @@ void print_triangle(int size)
 
 	if (size > 0)
 	{
-		for (i = 0 ; i < size ; i++)
-		{
-			for (j = 0 ; j < size ; j++)
-			{
-				if (j < size - (i + 1))
-				{
-					_putchar(' ');
-				}
-				else
-					_putchar('#');
-			}
-			_putchar('\n');
-		}
-				_putchar('\n');
+	for (i = 0 ; i < size ; i++)
+	{
+	for (j = 0 ; j < size ; j++)
+	{
+	if (j < size - (i + 1))
+	{
+		_putchar(' ');
+	}
+	else
+	{
+		_putchar('#');
+	}
+	}
+	_putchar('\n');
+	}
+	}
+	else
+	{
+		_putchar('\n');
 	}
 }
