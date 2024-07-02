2c592ebe4801988c14449b7a19955a6305ee31ac
MannyAdewumigit config --global user.name MannyAdewumi <mannyadewumi@gmail.com>
2024-07-01 20:32:59 -0700

    task 8 done

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints array
+ * @a: value 1
+ *@n: value 2
+ *Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(" , ");
+	}
+	printf("\n");
+}
