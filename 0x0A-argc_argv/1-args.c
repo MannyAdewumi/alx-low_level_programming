977026089a02d7027a421eabf118511b3e4ac04e
MannyAdewumigit config --global user.name MannyAdewumi <mannyadewumi@gmail.com>
2024-07-13 12:12:21 -0700

    1-argc complete

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main- prints name
+ * @argc: arguement count
+ * @argv: arguement vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
