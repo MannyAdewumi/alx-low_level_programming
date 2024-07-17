62975be57f051ccb76ca33418f8529188c631861
MannyAdewumigit config --global user.name MannyAdewumi <mannyadewumi@gmail.com>
2024-07-17 09:33:21 -0700

    not bragging

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *free_grid -
+ *@grid:pointer to array
+ *@height:pointer to row
+ *Return: pointer to return
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
