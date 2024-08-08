42ca1f128693bd9ad9fa8f19d5f3c8b0ad2d2456
MannyAdewumigit config --global user.name MannyAdewumi <mannyadewumi@gmail.com>
2024-08-08 09:16:19 -0700

    changing bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * flip_bits - checks the number of bits to be changed
+ * @n: start num
+ * @m: end num
+ * Return: the number of bits to be changed
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int l, r = 0;
+	unsigned long int prev;
+	unsigned long int x = n ^ m;
+
+	for (l = 63; l >= 0; l--)
+	{
+		prev = x >> l;
+
+		if (prev & 1)
+			r++;
+	}
+	return (rounder);
+}
