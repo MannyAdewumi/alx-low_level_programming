088e3b228eb4544d87c68d4f21d4fbae61f44502
MannyAdewumigit config --global user.name MannyAdewumi <mannyadewumi@gmail.com>
2024-09-19 03:41:16 -0700

    list of elements

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_dlistint - function to print elements
+ *
+ * @h: pointer to head of list
+ * Return: number of nodes in list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t elem_count = 0;
+
+	while (h)
+	{
+		elem_count += 1;
+		h = h->next;
+	}
+	return (elem_count);
+}
