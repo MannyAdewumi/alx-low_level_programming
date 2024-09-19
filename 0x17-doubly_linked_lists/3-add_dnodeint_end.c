b027a1e30ae386312a5bfa141ea6207c5e006fa0
MannyAdewumigit config --global user.name MannyAdewumi <mannyadewumi@gmail.com>
2024-09-19 06:27:31 -0700

    add node at end

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ * add_dnodeint_end:function to add a new node at the end of list
+ * @n: data to be returned
+ * @head: pointer to head of list
+ * Return: pointer to new node
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new, *tmp;
+
+	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	tmp = (*head);
+
+	if (tmp != NULL)
+	{
+		while (tmp->next != NULL)
+		{
+			tmp = tmp->next;
+		}
+		tmp->next = new;
+	}
+	else
+		*head = new;
+	new->prev = tmp;
+
+	return (new);
+}
